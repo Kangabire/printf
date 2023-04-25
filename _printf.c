bc8c7fb57702475c139f25aacdbcb30f9a7be6b8
KangabireMuhozaMerveille <126694465+KangabireMuhozaMerveille@users.noreply.github.com>
2023-04-26 00:41:57 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -3,7 +3,7 @@
 
 /**
 <<<<<<< HEAD
- * check_for_specifiers - checks if there is a valid format specifier
+ * check_for_specifiers - check if there is a valid format specifier
  * @format: possible format specifier
  *
  * Return: pointer to valid function or NULL
