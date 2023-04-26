2339f3d38f72a125e4768e28475382db59a37512
KangabireMuhozaMerveille <kangabirem@gmail.com>
2023-04-26 13:29:36 -0700

    print

--- case_i.c
+++ case_i.c
@@ -10,26 +10,27 @@
  */
 int int_handler(int value)
 {
-        int num_chars = 0;
-        int char_count = 0;
-        char int_str[20];
-        int i;
+	int num_chars = 0;
+	int char_count = 0;
+	char int_str[20];
+	int i;
 
-        if (value < 0)
-        {
-        _putchar('-');
-       	char_count++;
-        value = -value;
-        }
-        do {
-        int_str[num_chars++] = (value % 10) + '0';
-        value /= 10;
-        } while (value != 0);
+	if (value < 0)
+	{
+	_putchar('-');
+	char_count++;
+	value = -value;
+	}
+	do {
+	int_str[num_chars++] = (value % 10) + '0';
+	value /= 10;
+	}
 
-        for (i = num_chars - 1; i >= 0; i--)
-        {
-        _putchar(int_str[i]);
-        char_count++;
-        }
-        return (char_count);
+	while (value != 0);
+	for (i = num_chars - 1; i >= 0; i--)
+	{
+	_putchar(int_str[i]);
+	char_count++;
+	}
+	return (char_count);
 }
